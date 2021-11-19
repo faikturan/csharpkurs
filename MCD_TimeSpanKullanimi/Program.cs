e8cb8ce0ef049b18070b081714d4785c356d157d
Faik Turan <turanfaik@hotmail.com>
2021-11-19 15:55:23 +0300

    Csharp Development Architect Education

---
+++ MCD_TimeSpanKullanimi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MCD_TimeSpanKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TimeSpan : ZamanAralığı DateTime tipindeki değişkenlerin matematiksel işlemlerinde kullanılıyor.
+            TimeSpan zamanAraligi = new TimeSpan();
+            Console.WriteLine("Doğum Tarihi giriniz: dd.MM.yyyy");
+
+            DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", null, DateTimeStyles.None, out DateTime dogumTarihi);
+
+            zamanAraligi = DateTime.Now - dogumTarihi;
+            Console.WriteLine("Kaç gündür yaşıyormuş? = " +(int)zamanAraligi.TotalDays);
+            Console.WriteLine($"Kaç yaşındaymış? = {(int)(zamanAraligi.TotalDays /365)} ");
+            Console.WriteLine("Kaç saattir yaşıyormuş? {0}", (int)zamanAraligi.TotalHours);
+
+            Console.ReadKey();
+
+        }
+    }
+}
